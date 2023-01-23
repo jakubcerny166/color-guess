e5bbbfbaa1c4fee4a371ed6f4d7811a5b6caa84e
Jakub Černý <kubiik.c@seznam.cz>
2023-01-23 18:53:46 +0100

    final commit

---
+++ script.js
@@ -0,0 +1,39 @@
+var arr =[0,1,2];
+var r= Math.floor((Math.random())*256);
+var g= Math.floor((Math.random())*256);
+var b= Math.floor((Math.random())*256);
+
+document.getElementById("cName").innerHTML="( "+r+" , "+g+" , "+b+" )";
+
+var index=Math.floor((Math.random())*3);
+
+document.getElementById(index).style.backgroundColor="rgb("+r+","+g+","+b+")";
+
+arr.splice(index,1);
+
+var r1= Math.floor((Math.random())*256);
+var g1= Math.floor((Math.random())*256);
+var b1= Math.floor((Math.random())*256);
+document.getElementById(arr[0]).style.backgroundColor="rgb("+r1+","+g1+","+b1+")";
+
+var r2= Math.floor((Math.random())*256);
+var g2= Math.floor((Math.random())*256);
+var b2= Math.floor((Math.random())*256);
+document.getElementById(arr[1]).style.backgroundColor="rgb("+r2+","+g2+","+b2+")";
+
+function guess(){
+    if(document.activeElement.id==index){
+        document.getElementById("cName").innerHTML="CORRECT !!";
+        document.getElementById("cName").style.color="darkgreen";
+    }
+    else{
+        document.getElementById("cName").innerHTML="WRONG !!";
+        document.getElementById("cName").style.color="red";
+    }
+    document.getElementsByClassName("btn")[0].removeAttribute("onclick");
+    document.getElementsByClassName("btn")[1].removeAttribute("onclick");
+    document.getElementsByClassName("btn")[2].removeAttribute("onclick");
+}
+function restart(){
+    window.location.reload();
+}
